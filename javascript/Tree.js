308572a85ec37f4cc8a317485014bcc3ee6ec120
MichaelDulin <michaeldulin1211@gmail.com>
2023-04-21 23:35:58 +0000

    added tree class

---
+++ javascript/Tree.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+
+}
+
+class BinarySearchTree extends BinaryTree{
+
+}
+
+
