7e32ee35a29cc8a17393879e72ff66bace929b6f
MichaelDulin <michaeldulin1211@gmail.com>
2023-04-25 02:32:18 +0000

    implemented add function for BST

--- javascript/Tree.js
+++ javascript/Tree.js
@@ -9,11 +9,77 @@ class Node {
 }
 
 class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
 
+  preOrder() {
+    const treeArr = [];
+    const recurse = (root) => {
+      treeArr.push(root.value);
+      if (root.left) {
+        recurse(root.left);
+      }
+      if (root.right) {
+        recurse(root.right);
+      }
+    };
+    recurse(this.root);
+    return(treeArr);
+  }
+
+
+  inOrder() {
+    const treeArr = [];
+    const recurse = (root) => {
+      if(root.left) {
+        recurse(root.left);
+      }
+      treeArr.push(root.value);
+      if (root.right) {
+        recurse(root.right);
+      }
+    };
+    recurse(this.root);
+    return treeArr;
+  }
+
+  postOrder() {
+    const treeArr = [];
+    const recurse = (root) => {
+      root.left ? recurse(root.left) : root.right ? recurse(root.right) : treeArr.push(root.value)
+    };
+    recurse(this.root);
+    return treeArr;
+  }
 }
 
-class BinarySearchTree extends BinaryTree{
 
+class BinarySearchTree extends BinaryTree{
+  add(value) {
+    // Return: nothing
+    let node = new Node(value);
+    const addNode = (curNode, newNode) => {
+      if (curNode.value > newNode.value) {
+        if (curNode.left === null) {
+          return curNode.left = newNode;
+        } else {
+          addNode(curNode.left, newNode);
+        }
+      } else {
+        if (curNode.right === null) {
+          return curNode.right = newNode;
+        } else {
+          addNode(curNode.right, newNode);
+        }
+      }
+    };
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      addNode(this.root, node);
+    }
+  }
 }
 
 
