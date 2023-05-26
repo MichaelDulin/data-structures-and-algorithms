1e3efe45630d3c549fc9c5d7f12e86550d2a81f6
MichaelDulin <michaeldulin1211@gmail.com>
2023-05-25 17:16:19 -0700

    fixed parent graph class and implemented depth first

--- javascript/Graph/Graph.js
+++ javascript/Graph/Graph.js
@@ -1,28 +1,98 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.edges = [];
+    this.edges = new Map();
   }
 }
 
 class Graph {
   constructor() {
-    this.nodes = [];
+    this.nodes = new Map();
   }
 
   addNode(value) {
-    const newNode = new Node(value);
-    this.nodes.push(newNode);
-    return newNode;
+    if (!this.nodes.has(value)) {
+      this.nodes.set(value, new Node(value));
+    } else {
+      console.log('Node already exists!');
+    }
   }
 
-  addEdge(node1, node2, weight = null) {
-    if (!this.nodes.includes(node1) || !this.nodes.includes(node2)) {
-      throw new Error("Both nodes should already be in the graph");
+  addEdge(node1Value, node2Value, edgeValue) {
+    let node1 = this.nodes.get(node1Value);
+    let node2 = this.nodes.get(node2Value);
+
+    if (node1 && node2) {
+      node1.edges.set(node2Value, edgeValue);
+      node2.edges.set(node1Value, edgeValue);
+    } else {
+      console.log('One or both nodes do not exist!');
+    }
+  }
+
+  printGraph() {
+    this.nodes.forEach((node, nodeValue) => {
+      let edgesString = "";
+      node.edges.forEach((edgeValue, adjacentNodeValue) => {
+        edgesString += adjacentNodeValue + '(' + edgeValue + ') ';
+      });
+      console.log(nodeValue + ' -> ' + edgesString);
+    });
+  }
+
+  breadthFirst(startNodeValue) {
+    let startNode = this.nodes.get(startNodeValue);
+
+    if (!startNode) {
+      console.log('Start node does not exist!');
+      return;
     }
 
-    node1.edges.push({ node: node2, weight });
-    node2.edges.push({ node: node1, weight });
+    let visited = new Set();
+    let queue = [startNode];
+
+    while (queue.length > 0) {
+      let currentNode = queue.shift();
+
+      console.log(currentNode.value);
+
+      visited.add(currentNode.value);
+
+      currentNode.edges.forEach((edgeValue, adjacentNodeValue) => {
+        let adjacentNode = this.nodes.get(adjacentNodeValue);
+
+        if (!visited.has(adjacentNode.value)) {
+          queue.push(adjacentNode);
+        }
+      });
+    }
+  }
+
+  depthFirst(startNodeValue) {
+    let startNode = this.nodes.get(startNodeValue);
+
+    if (!startNode) {
+      console.log('Start node does not exist!');
+      return;
+    }
+
+    let visited = new Set();
+
+    this._dfsRecursive(startNode, visited);
+  }
+
+  recDepthFirst(node, visited) {
+    console.log(node.value);
+
+    visited.add(node.value);
+
+    node.edges.forEach((edgeValue, adjacentNodeValue) => {
+      let adjacentNode = this.nodes.get(adjacentNodeValue);
+
+      if (!visited.has(adjacentNode.value)) {
+        this._dfsRecursive(adjacentNode, visited);
+      }
+    });
   }
 
   getNodes() {
@@ -37,3 +107,5 @@ class Graph {
     return this.nodes.length;
   }
 }
+
+module.exports = Graph;
